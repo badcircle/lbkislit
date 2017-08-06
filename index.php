fa88825899653288757eef3b3e6f48d47bdfaff1
Adam Wood <adamwood@Adams-iMac.local>
2017-08-06 13:53:07 -0500

    current branch of thought

--- index.php
+++ index.php
@@ -8,17 +8,38 @@
     <link rel="stylesheet" href="css/app.css">
   </head>
   <body>
-    <div class="grid-container">
-      <div class="grid-x">
-        <div class="cell shrink">
-          <h1><img src="img/logo.png" style="width: 100px; margin-top: 1rem;" alt="Lubbock is Lit"></h1>
+    <div class="grid-x">
+      <div class="cell shrink" style="vertical-align: bottom;">
+        <h1><img src="img/logo.png" style="" alt="Lubbock is Lit" class="logo"></h1>
+      </div>
+      <div class="cell auto nav-wrap">
+        <div class="grid-x" data-equalizer>
+          <div class="cell medium-4" data-equalizer-watch>
+            <img src="img/nav/nav_01.png" alt="">
+          </div>
+          <div class="cell medium-4" data-equalizer-watch>
+            <div class="grid-x">
+              <div class="cell medium-12">
+                <img src="img/nav/nav_02.png" alt="">
+              </div>
+            </div>
+            <div class="grid-x">
+              <div class="cell medium-12">
+                <img src="img/nav/nav_04.png" alt="">
+              </div>
+            </div>
+          </div>
+          <div class="cell medium-4" data-equalizer-watch>
+            <img src="img/nav/nav_03.png" alt="">
+          </div>
         </div>
-        <div class="cell auto">
-          <ul class="nav">
-            <li><a href="#">Food</a></li>
-            <li><a href="#">Music</a></li>
-            <li><a href="#">Living in Lubbock</a></li>
-          </ul>
+        <div class="grid-x">
+          <div class="cell medium-7">
+            <img src="img/nav/nav_05.png" alt="">
+          </div>
+          <div class="cell medium-5">
+            <img src="img/nav/nav_06.png" alt="">
+          </div>
         </div>
       </div>
     </div>
