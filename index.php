f8ff815abb9eed59ae9c56ee4660f8137b5fccce
Adam Wood <wood@badcircle.com>
2017-08-07 08:24:15 -0500

    optimzed

--- index.php
+++ index.php
@@ -8,23 +8,23 @@
     <link rel="stylesheet" href="css/app.css">
   </head>
   <body>
-    <img src="img/logo.png" class="logo" alt="">
+    <img src="img/logo-fs8.png" class="logo" alt="">
     <div class="grid-container">
     <div class="grid-x">
       <div class="cell auto nav-wrap">
         <div class="grid-x">
-          <div class="cell cell-art"><img src="img/nav/nav_01.png" alt=""></div>
+          <div class="cell cell-art"><img src="img/nav/nav_01-fs8.png" alt=""></div>
           <div class="cell cell-music-food">
             <div class="grid-x">
-              <div class="cell cell-food"><img src="img/nav/nav_02.png" alt=""></div>
-              <div class="cell cell-music"><img src="img/nav/nav_04.png" alt=""></div>
+              <div class="cell cell-food"><img src="img/nav/nav_02-fs8.png" alt=""></div>
+              <div class="cell cell-music"><img src="img/nav/nav_04-fs8.png" alt=""></div>
             </div>
           </div>
-          <div class="cell cell-living"><img src="img/nav/nav_03.png" alt=""></div>
+          <div class="cell cell-living"><img src="img/nav/nav_03-fs8.png" alt=""></div>
         </div>
         <div class="grid-x">
-          <div class="cell cell-rec"><img src="img/nav/nav_05.png" alt=""></div>
-          <div class="cell cell-features"><img src="img/nav/nav_06.png" alt=""></div>
+          <div class="cell cell-rec"><img src="img/nav/nav_05-fs8.png" alt=""></div>
+          <div class="cell cell-features"><img src="img/nav/nav_06-fs8.png" alt=""></div>
         </div>
       </div>
     </div>
