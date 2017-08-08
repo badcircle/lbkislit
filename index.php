d6cfe5830d1d2014289097109ce4bcff64092d3b
Adam Wood <adam.wood@ttuhsc.edu>
2017-08-08 11:18:55 -0500

    isolated header

--- index.php
+++ index.php
@@ -8,27 +8,7 @@
     <link rel="stylesheet" href="css/app.css">
   </head>
   <body>
-    <img src="img/logo-fs8.png" class="logo" alt="">
-    <div class="grid-container">
-    <div class="grid-x">
-      <div class="cell auto nav-wrap">
-        <div class="grid-x">
-          <div class="cell cell-art"><img src="img/nav/nav_01-fs8.png" alt=""></div>
-          <div class="cell cell-music-food">
-            <div class="grid-x">
-              <div class="cell cell-food"><img src="img/nav/nav_02-fs8.png" alt=""></div>
-              <div class="cell cell-music"><img src="img/nav/nav_04-fs8.png" alt=""></div>
-            </div>
-          </div>
-          <div class="cell cell-living"><img src="img/nav/nav_03-fs8.png" alt=""></div>
-        </div>
-        <div class="grid-x">
-          <div class="cell cell-rec"><img src="img/nav/nav_05-fs8.png" alt=""></div>
-          <div class="cell cell-features"><img src="img/nav/nav_06-fs8.png" alt=""></div>
-        </div>
-      </div>
-    </div>
-    </div>
+    <?php include(".\header.php"); ?>
 
     <div class="grid-container" id="intro">
       <div class="grid-x">
